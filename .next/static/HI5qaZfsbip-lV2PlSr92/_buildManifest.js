7374de35a01fc2cf73ddda928d806cdb488dc585
gabrielbarbosaa <gbarbosa1602@gmail.com>
2022-11-29 16:13:55 -0300

    config git ignore

--- .next/static/HI5qaZfsbip-lV2PlSr92/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/77-e09726534aa2a5d6.js","static/chunks/pages/index-5cc7394db7b93960.js"],"/_error":["static/chunks/pages/_error-8353112a01355ec2.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
