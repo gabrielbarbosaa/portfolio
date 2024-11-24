b1c71a80e11aa161d6aaf02ce8d13a8887f7b978
Gabriel Barbosa Azevedo <gbarbosa1602@gmail.com>
2024-11-24 14:40:47 -0300

    feat: updat eslint and next app

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -2,12 +2,61 @@ import globals from "globals";
 import pluginJs from "@eslint/js";
 import tseslint from "typescript-eslint";
 import pluginReact from "eslint-plugin-react";
-
+import restrictedGlobals from "./.eslint/restricted-globals.cjs";
 
 /** @type {import('eslint').Linter.Config[]} */
 export default [
-  {files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"]},
-  {languageOptions: { globals: globals.browser }},
+  {
+    files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"],
+  },
+  {
+    languageOptions: {
+      globals: {
+        ...globals.browser,
+        module: "readonly",
+      },
+      ecmaVersion: 2024, // Keep this here
+      sourceType: "module",
+    },
+    settings: {
+      react: {
+        version: "detect",
+      },
+    },
+    rules: {
+      "react/jsx-no-target-blank": "off",
+      "no-restricted-globals": ["error"].concat(restrictedGlobals),
+      "no-restricted-imports": [
+        "error",
+        {
+          patterns: ["@/config/*", "@/features/*/*", "@/shared/*/*"],
+        },
+      ],
+      "@typescript-eslint/ban-ts-comment": [
+        "error",
+        {
+          "ts-expect-error": "allow-with-description",
+        },
+      ],
+      "@typescript-eslint/no-empty-function": "off",
+      "@typescript-eslint/no-empty-interface": "off",
+      "@typescript-eslint/no-explicit-any": [
+        "warn",
+        {
+          ignoreRestArgs: true,
+        },
+      ],
+      "no-unused-vars": "off",
+      "@typescript-eslint/no-unused-vars": [
+        "warn",
+        {
+          argsIgnorePattern: "^_",
+          destructuredArrayIgnorePattern: "^_",
+          ignoreRestSiblings: true,
+        },
+      ],
+    },
+  },
   pluginJs.configs.recommended,
   ...tseslint.configs.recommended,
   pluginReact.configs.flat.recommended,
---
+++ src/app/page.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function Home() {
+  return (
+    <div>
+      <h1>Olar!</h1>
+    </div>
+  );
+}
